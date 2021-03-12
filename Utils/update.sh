9d14536840b6a726963179c1acc2e5601b01d882
Benjamin Hättasch <benjamin.haettasch@cs.tu-darmstadt.de>
2021-03-12 22:51:44 +0100

    Add util scripts and production settings

---
+++ Utils/update.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# Update
+# execute as Utils/update.sh
+
+# abort on error, print executed commands
+set -ex
+
+# activate virtualenv if necessary
+if [ -z ${VIRTUAL_ENV+x} ]; then
+    source venv/bin/activate
+fi
+
+# set environment variable when we want to update in production
+if [ "$1" = "--prod" ]; then
+    export DJANGO_SETTINGS_MODULE=bptool.settings_production
+fi
+
+# before potentially breaking anything, create a data backup
+mkdir -p backups/
+python manage.py dumpdata --indent=2 > "backups/$(date +"%Y%m%d%H%M")_datadump.json" --traceback
+
+git pull
+pip install --upgrade setuptools pip wheel
+pip install --upgrade -r requirements.txt
+
+./manage.py migrate
+./manage.py collectstatic --noinput
+./manage.py compilemessages -l de_DE
+
+touch bptool/wsgi.py
---
+++ bptool/settings_production.py
@@ -0,0 +1,36 @@
+"""
+This is the settings file used in production.
+First, it imports all default settings, then overrides respective ones.
+Secrets are stored in and imported from an additional file, not set under version control.
+"""
+
+import bptool.settings_secrets as secrets
+
+# noinspection PyUnresolvedReferences
+from bptool.settings import *
+
+### SECURITY ###
+
+DEBUG = False
+
+ALLOWED_HOSTS = secrets.HOSTS
+
+SECRET_KEY = secrets.SECRET_KEY
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+### DATABASE ###
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': getattr(secrets, "DB_HOST", "localhost"),
+        'NAME': secrets.DB_NAME,
+        'USER': secrets.DB_USER,
+        'PASSWORD': secrets.DB_PASSWORD,
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
+    }
+}
