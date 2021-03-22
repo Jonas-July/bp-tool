dfa73316cbe817ed86e5d07bdad227deabb38c92
Benjamin Haettasch <benjamin.haettasch@cs.tu-darmstadt.de>
2021-03-22 22:27:21 +0100

    Fix postgres settings for production setuo

--- bptool/settings_production.py
+++ bptool/settings_production.py
@@ -26,11 +26,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'HOST': getattr(secrets, "DB_HOST", "localhost"),
+        'PORT': getattr(secrets, "DB_PORT", "5432"),
         'NAME': secrets.DB_NAME,
         'USER': secrets.DB_USER,
         'PASSWORD': secrets.DB_PASSWORD,
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
     }
 }
