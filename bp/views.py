6d4aeb4b295980bcb920f541c0ed476246789f88
Benjamin Hättasch <benjamin.haettasch@cs.tu-darmstadt.de>
2021-11-17 14:12:18 +0100

    Minor UX improvements and speedup
    Reduce database load for TL view by prefetching current problems
    Mark mailto links with envelope symbol
    Link to TL detail view instead of TL mail on project page

--- bp/views.py
+++ bp/views.py
@@ -108,7 +108,7 @@ class TLView(PermissionRequiredMixin, DetailView):
         context["logs_count"] = context["logs"].count()
         context["logs_count_good"] = context["tl"].tllog_set.filter(good_log=True).count()
         context["logs_count_bad"] = context["tl"].tllog_set.filter(good_log=False).count()
-        context["projects"] = context["tl"].project_set.all()
+        context["projects"] = context["tl"].project_set.all().prefetch_related("tllog_set", "tllog_set__current_problems")
         return context
 
 
