0ac42ebe1f32f54c6f4328113c2b3bddeab3e2c8
Jonas-July <jonas.july@outlook.de>
2022-08-27 17:22:44 +0200

    Connect students with respective model
    A list of prospective students is pre-loaded by hand.
    Students are matched using their exact email address which is unique for each student.
    
    If the student wasnt found in the list, then show a message.

--- bptool/lti/views.py
+++ bptool/lti/views.py
@@ -1,6 +1,7 @@
+from django.contrib import messages
 from lti_provider.views import LTIRoutingView
 
-from bp.views import BP, TL
+from bp.views import BP, TL, Student
 
 class TLRoutingView(LTIRoutingView):
     def dispatch(self, request, *args, **kwargs):
@@ -10,4 +11,18 @@ class TLRoutingView(LTIRoutingView):
         if instance and not TL.objects.filter(user=instance).first():
             TL.objects.create(user=instance, name=f"{instance.first_name} {instance.last_name}", bp=BP.get_active(),
                               confirmed=False)
+        return response
+
+class StudentRoutingView(LTIRoutingView):
+    def dispatch(self, request, *args, **kwargs):
+        response = super().dispatch(request, *args, **kwargs)
+        # Login happens in dispatch above
+        instance = request.user
+        if instance and instance.email and not Student.objects.filter(user=instance).first():
+            associatedStudent = Student.objects.filter(mail=instance.email, bp=BP.get_active()).first()
+            if associatedStudent:
+                associatedStudent.user = instance
+                associatedStudent.save()
+            else:
+                messages.add_message(self.request, messages.WARNING, "E-Mail-Addresse nicht gefunden. Bitte wende dich an die Veranstalter.")
         return response
