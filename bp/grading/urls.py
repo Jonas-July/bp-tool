8b3e93f4e8d06d8e2db86a6a0b3d689ae1d1e0c0
Jonas-July <jonas.july@outlook.de>
2022-09-22 15:30:08 +0200

    Change order of urls
    URLs use the first-fit approach, so more general urls need to come later

--- bp/grading/urls.py
+++ bp/grading/urls.py
@@ -4,11 +4,11 @@ from django.views.generic import TemplateView
 from .ag.views import AGGradeView, AGGradeSuccessView, AGGradeEarlyView
 
 aggrade_patterns = [
-    path('<str:order_id>/<str:secret>/', AGGradeView.as_view(), name="ag_grade"),
-    path('<str:order_id>/success/', AGGradeSuccessView.as_view(), name="ag_grade_success"),
-    path('<str:order_id>/early/', AGGradeEarlyView.as_view(), name="ag_grade_too_early"),
     path('invalid/', TemplateView.as_view(template_name="bp/project_grade_invalid_secret.html"),
          name="ag_grade_invalid"),
+    path('<str:order_id>/early/', AGGradeEarlyView.as_view(), name="ag_grade_too_early"),
+    path('<str:order_id>/success/', AGGradeSuccessView.as_view(), name="ag_grade_success"),
+    path('<str:order_id>/<str:secret>/', AGGradeView.as_view(), name="ag_grade"),
 ]
 
 grading_patterns = [
