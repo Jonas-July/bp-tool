6060f4e7bdc01b53b7fb11ce2e712e5edde7f494
Jonas-July <jonas.july@outlook.de>
2022-09-22 15:39:14 +0200

    Improved AG grading form
    AG points are checked on client-side aswell
    New checks concerning missing keys due to invalid inputs from client

--- bp/grading/ag/forms.py
+++ bp/grading/ag/forms.py
@@ -15,7 +15,8 @@ class AGGradeForm(forms.ModelForm):
         model = AGGradeBeforeDeadline
         fields = ['ag_points', 'ag_points_justification', 'project']
         widgets = {
-            'project' : forms.HiddenInput()
+            'project'   : forms.HiddenInput(),
+            'ag_points' : forms.NumberInput(attrs={'min': 0, 'max': 100, 'step' : 1, 'required': True, 'type': 'number',})
         }
 
     # Additional fields for information of AG (which project am I currently grading?) and authentication
@@ -29,17 +30,17 @@ class AGGradeForm(forms.ModelForm):
         cleaned_data = super().clean()
 
         # Make sure the AG entered a justification and a valid number of points
-        if cleaned_data['ag_points_justification'] == "":
-            self.add_error('ag_points_justification', "Bitte geben Sie eine Begründung ein")
-        if not (0 <= cleaned_data['ag_points'] <= 100):
+        # Django already checks if a justification was given and shows error
+        # Same with ag_points. If they don't exist here, then Django has detected the issue
+        if not (0 <= cleaned_data.get('ag_points', 0) <= 100):
             self.add_error('ag_points', "Die Punktzahl muss zwischen 0 und 100 liegen")
 
         # Check validity of secret using Pretix
-        try:
-            secret = get_order_secret(cleaned_data['project'].order_id)
-        except KeyError:
+        project = cleaned_data.get('project', None)
+        if not project:
             raise ValidationError("Das Projekt ist unbekannt. Ihre Eingabe wurde nicht gespeichert.")
-        if cleaned_data['secret'] != secret:
+        secret = get_order_secret(project.order_id)
+        if not cleaned_data.get('secret', None) or cleaned_data['secret'] != secret:
             raise ValidationError("Authentifizierung fehlgeschlagen. Ihre Eingabe wurde nicht gespeichert.")
 
         return cleaned_data
@@ -64,7 +65,7 @@ class AGGradeForm(forms.ModelForm):
                 f"""Es wurde eine neue Bewertung für Ihre Gruppe '{project}' eingetragen. Sie erhalten diese E-Mail, da die Deadline zur Bewertung überschritten wurde.
 
 Die neue Bewertung wurde gespeichert, die Änderungen können aber nur nach entsprechender Absprache mit den Veranstaltern berücksichtigt werden. 
-Falls noch nicht geschehen, nehmen Sie daher bitte Kontakt zum BP Orga Team (bp@cs.tu-darmstadt.de) auf.""",
+Falls noch nicht geschehen, nehmen Sie daher bitte Kontakt zum BP Orga Team ({settings.SEND_MAILS_TO}) auf.""",
                 f"Sent via BP-Tool <{settings.SEND_MAILS_FROM}>",
                 [project.ag_mail]
             )
