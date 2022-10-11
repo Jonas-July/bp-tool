843eb12c20db8115e957e68236c812fbed7172f6
Jonas-July <jonas.july@outlook.de>
2022-10-11 11:34:58 +0200

    Move Orgalog related files
    Moves the following into the orgalog dir:
    - model OrgaLog
    - project info tags for the orgalogs on the project view
    - templates for tags, rendering and orgalog view
    
    Minor fix in render_orgalogs; renders link to detail page again

--- bp/orgalogs/orga/views.py
+++ bp/orgalogs/orga/views.py
@@ -3,9 +3,11 @@ from django.views.generic import DetailView
 
 from bp.models import OrgaLog
 
+# necessary to load the project info tags
+from .project_info_tags import *
 
 class OrgaLogView(PermissionRequiredMixin, DetailView):
     model = OrgaLog
-    template_name = "bp/orgalog.html"
+    template_name = "bp/orgalogs/orga/orgalog.html"
     context_object_name = "log"
     permission_required = "bp.view_orgalog"
