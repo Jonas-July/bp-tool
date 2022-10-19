307bb7d410ddaca3f5528bba60009dba93c4e9f4
Jonas-July <jonas.july@outlook.de>
2022-10-19 22:21:04 +0200

    Move grading related code into existing module
    Project info tags relating to grading are imported to empty view
    The empty view is imported into the urls package to ensure that it is loaded at startup.
    This is all necessary to register the tags at startup
    
    The templates are all renamed only (with modifications re. renaming),
    despite some being registered as deleted and added

---
+++ bp/grading/orga/project_info_tags.py
@@ -0,0 +1,25 @@
+from bp.templatetags.tags_project_info_misc import ProjectInfoTabTags
+from bp.templatetags.tags_project_info_table import ProjectInfoTable
+
+from ..mixins import ProjectGradesMixin
+
+grading_tab = ProjectInfoTabTags(priority=3*128)
+
+@grading_tab.register_description('bp/grading/orga/project_info_misc_grading_desc.html')
+def grading_description(project):
+    tab_context = {'project' : project}
+    return ProjectGradesMixin.get_grading_context_data(tab_context, project)
+
+@grading_tab.register_content('bp/grading/orga/project_info_misc_grading_content.html')
+def grading_content(project):
+    tab_context = {'project' : project}
+    return ProjectGradesMixin.get_grading_context_data(tab_context, project)
+
+
+@ProjectInfoTable.register('bp/grading/orga/project_info_table_grade_info.html', priority=5)
+def grade_info(project):
+    return {
+        'show_aggrade'            : project.ag_points >= 0,
+        'ag_points'               : project.ag_points,
+        'ag_points_justification' : project.ag_points_justification,
+    }
