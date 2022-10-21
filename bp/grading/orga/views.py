c12d1679955e8df5894ff704a808f4b016ad920c
Jonas-July <jonas.july@outlook.de>
2022-10-21 17:06:50 +0200

    Make grade import compatible with 3.9
    match statement isn't supported in 3.9, so replace them with if/else.

--- bp/grading/orga/views.py
+++ bp/grading/orga/views.py
@@ -23,6 +23,55 @@ class OrgaGradesImportView(LoginRequiredMixin, FormView):
     success_url = reverse_lazy("bp:project_list")
 
     def form_valid(self, form):
+        """
+        For a version using the match statement, see below
+        """
+        import_count = 0
+        separator = ','
+        reader = csv.DictReader(io.TextIOWrapper(form.cleaned_data.get("csvfile").file), delimiter=separator)
+        active_bp = BP.get_active()
+        lines_ignored = defaultdict(lambda:0)
+        for row in reader:
+            if 'nr' in row and 'justification' in row:
+                nr, justification = row["nr"], row["justification"]
+                project = active_bp.project_set.filter(nr=nr).first()
+                if not project:
+                    print(f"Project mit Nummer '{nr}' existiert nicht")
+                    lines_ignored["Projekt existiert nicht"] += 1
+                    continue
+                if 'pitch_grade' in row or 'docs_grade' in row:
+                    grade_points = row['pitch_grade'] if 'pitch_grade' in row else row['docs_grade']
+                    grade_type = PitchGrade if 'pitch_grade' in row else DocsGrade
+                    try:
+                        grade_type.objects.create(project=project,
+                                              grade_points=grade_points,
+                                              grade_justification=justification)
+                    except IntegrityError:
+                        print(f"Bewertung des Typs {grade_type} für Projekt {project} existiert bereits")
+                        lines_ignored["Bewertung existiert bereits"] += 1
+                        continue
+                    except ValidationError:
+                        lines_ignored[f"Ungültiger Wert für die Bewertung (ValidationError)"] += 1
+                        continue
+                    else:
+                        import_count += 1
+                else:
+                    lines_ignored["Spalten 'pitch_grade' und 'docs_grade' nicht gefunden"] += 1
+                    continue
+            else:
+                lines_ignored["Spalten 'nr' und/oder 'justification' nicht gefunden"] += 1
+                continue
+        messages.add_message(self.request, messages.SUCCESS, f"{import_count} Bewertung(en) erfolgreich importiert")
+        for error_msg, ignored_lines in lines_ignored.items():
+            messages.add_message(self.request, messages.WARNING, f"{ignored_lines} Zeile(n) ignoriert wegen: {error_msg}")
+        return super().form_valid(form)
+
+    '''
+    def form_valid_py3p10(self, form):
+        """
+        This version of form_valid uses the match statement
+        which is only available in 3.10+
+        """
         import_count = 0
         separator = ','
         reader = csv.DictReader(io.TextIOWrapper(form.cleaned_data.get("csvfile").file), delimiter=separator)
@@ -67,3 +116,4 @@ class OrgaGradesImportView(LoginRequiredMixin, FormView):
         for error_msg, ignored_lines in lines_ignored.items():
             messages.add_message(self.request, messages.WARNING, f"{ignored_lines} Zeile(n) ignoriert wegen: {error_msg}")
         return super().form_valid(form)
+    '''
