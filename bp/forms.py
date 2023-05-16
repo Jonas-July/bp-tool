f789693f272b30309dbf2437e23b960412bece9a
Nick Lange <nicklange685@gmail.com>
2023-05-16 17:07:20 +0200

    *added examples and better explanation for imports

--- bp/forms.py
+++ bp/forms.py
@@ -2,10 +2,8 @@ from django import forms
 
 
 class ProjectImportForm(forms.Form):
-    csvfile = forms.FileField(label="Projektliste (CSV)",
-                      help_text="CSV-Datei Semikolon-Separiert. Muss die Spalten nr, ag, ag_mail, title, order_id enthalten")
+    csvfile = forms.FileField(label="Projektliste (CSV)")
 
 
 class StudentImportForm(forms.Form):
-    csvfile = forms.FileField(label="Teilnehmendenliste (CSV)",
-                      help_text="CSV-Datei Komma-Separiert. Muss die Spalten ID, Vollständiger Name, E-Mail-Adresse, Gruppe enthalten")
+    csvfile = forms.FileField(label="Teilnehmendenliste (CSV)")
--- bp/grading/orga/forms.py
+++ bp/grading/orga/forms.py
@@ -12,7 +12,4 @@ class OrgaGradeCsvImportSpecification(Enum):
 
 class OrgaGradesImportForm(forms.Form):
     Spec = OrgaGradeCsvImportSpecification
-    csvfile = forms.FileField(label="Projektliste (CSV)",
-                      help_text=f"""CSV-Datei Komma-Separiert. \
-Muss die Spalten '{Spec.PROJECT.value}' und '{Spec.NOTES.value}' enthalten, \
-sowie entweder die Spalte '{Spec.PITCH_GRADE.value}' oder die Spalte '{Spec.DOCS_GRADE.value}'""")
+    csvfile = forms.FileField(label="Projektliste (CSV)")
