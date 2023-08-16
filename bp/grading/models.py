3f6e270bc56ef483af8e833ee1c99e1a4a2665f6
Nick Lange <nicklange685@gmail.com>
2023-08-16 13:25:23 +0200

    grade import/editing: - checks if points are within boundaries - added hint about point range to import info

--- bp/grading/models.py
+++ bp/grading/models.py
@@ -1,8 +1,11 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+
 
 class AGGrade(models.Model):
     class Meta:
         abstract = True
+
     project = models.ForeignKey("Project", on_delete=models.CASCADE)
     ag_points = models.SmallIntegerField(verbose_name="Punkte für den Implementierungsteil", help_text="0-100")
     ag_points_justification = models.TextField(verbose_name="Begründung")
@@ -16,6 +19,7 @@ class AGGrade(models.Model):
     def get_active():
         return TLLog.objects.filter(bp__active=True)
 
+
 class AGGradeBeforeDeadline(AGGrade):
     class Meta:
         verbose_name = "Bewertung"
@@ -24,6 +28,7 @@ class AGGradeBeforeDeadline(AGGrade):
 
     def __str__(self):
         return f"Bewertung für Projekt {self.project.nr} am {self.simple_timestamp}"
+
 
 class AGGradeAfterDeadline(AGGrade):
     class Meta:
@@ -34,6 +39,7 @@ class AGGradeAfterDeadline(AGGrade):
     def __str__(self):
         return f"Verspätete Bewertung für Projekt {self.project.nr} am {self.simple_timestamp}"
 
+
 class PitchGrade(models.Model):
     class Meta:
         verbose_name = "Bewertung der Vorträge"
@@ -41,11 +47,13 @@ class PitchGrade(models.Model):
         ordering = ['project']
 
     project = models.OneToOneField("Project", on_delete=models.CASCADE)
-    grade_points = models.DecimalField(verbose_name="Punkte für die Vorträge", max_digits=5, decimal_places=2)
+    grade_points = models.DecimalField(verbose_name="Punkte für die Vorträge", max_digits=5, decimal_places=2,
+                                       validators=[MaxValueValidator(20), MinValueValidator(0)])
     grade_notes = models.TextField(verbose_name="Notiz")
 
     def __str__(self):
         return f"Bewertung der Vorträge für Projekt {self.project.nr}"
+
 
 class DocsGrade(models.Model):
     class Meta:
@@ -54,7 +62,8 @@ class DocsGrade(models.Model):
         ordering = ['project']
 
     project = models.OneToOneField("Project", on_delete=models.CASCADE)
-    grade_points = models.DecimalField(verbose_name="Punkte für die Dokumentation", max_digits=5, decimal_places=2)
+    grade_points = models.DecimalField(verbose_name="Punkte für die Dokumentation", max_digits=5, decimal_places=2,
+                                       validators=[MaxValueValidator(80), MinValueValidator(0)])
     grade_notes = models.TextField(verbose_name="Notiz")
 
     def __str__(self):
